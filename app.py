9773e5a73ae1e6783bb5422fa25e63b52f7a946b
kondeti-chaithanya <kondeti.kumar@triconinfotech.com>
2026-06-02 16:02:49 +0530

    updated

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("Hello from Jenkins CI/CD Pipeline")
+def main():
+    # This is the message that Jenkins will capture in its build logs
+    print("Hello from Jenkins CI/CD Pipeline")
+
+if __name__ == "__main__":
+    main()
