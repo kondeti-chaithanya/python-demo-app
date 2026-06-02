4fc1b26436f6b9c9f47816138a607680d2b66d80
kondeti-chaithanya <kondeti.kumar@triconinfotech.com>
2026-06-02 16:11:55 +0530

    updated for serveres

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Jenkins CI/CD Demo App Running Successfully!"
+
+# Add this new route to catch the GitHub Webhook
+@app.route("/github-webhook/", methods=["POST"])
+def github_webhook():
+    print("Webhook received successfully from GitHub!")
+    return jsonify({"status": "received"}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
